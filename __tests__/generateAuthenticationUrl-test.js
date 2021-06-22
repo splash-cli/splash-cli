20c71bc14abc2911782b514e29ee82aac5513f1a
Rawnly <fedevitale99@gmail.com>
2021-06-22 20:03:21 +0200

    cleanup

--- __tests__/generateAuthenticationUrl-test.js
+++
@@ -1,8 +0,0 @@
-import { generateAuthenticationURL } from '../build/extra/utils'
-
-describe('generateAuthenticationURL', () => {
-	it('Should generate auth url', t => {
-		const url = generateAuthenticationURL('public', 'read_user')
-		expect( /\&scope=public\+read_user/g.test(url)).toBe(true)
-	})
-})
--- __tests__/pathFixer-test.js
+++
@@ -1,9 +0,0 @@
-import { pathFixer } from '../build/extra/utils'
-
-describe('PathFixer', () => {
-	it('Fix the path', t => {
-		const result = pathFixer('~/Desktop')
-
-		expect(result.includes('~')).toBe(false)
-	})
-})
